d02285417d2cb03e2defeb13245195eeb11c4e27
BobbieHoch <rwhochstadt@gmail.com>
2022-11-14 15:46:26 -0500

    changed class to id and made abc's two P's

--- Lab-7-MenuDOMCSS/menu.js
+++ Lab-7-MenuDOMCSS/menu.js
@@ -1,36 +1,34 @@
 const alphabet = document.getElementById("myalpha");
+const numbers = document.getElementById("mynums");
 
-console.log(alphabet);
+// console.log(alphabet);
+
 alphabet.addEventListener("click", showLetters);
 
-
 function showLetters() {
-    // alphabet.style.display = 'block';
-    numbers.style.display = 'none';
+    let alphabet = document.getElementById("myalpha");
+    let numbers = document.getElementById("mynums");
+    alphabet.style.display = "block";
+    numbers.style.display = "none";
 }
-showLetters();
-
-const numbers = document.getElementById("mynums");
+// showLetters();
 
 numbers.addEventListener("click", showNumbers);
 
 function showNumbers() {
-    numbers.style.display = 'block';
-    alphabet.style.display = 'none';
+    let alphabet = document.getElementById("myalpha");
+    let numbers = document.getElementById("mynums");
+    numbers.style.display = "block";
+    alphabet.style.display = "none";
 }
 
-const changeLable = document.querySelector("#btn");
+// const changeLable = document.querySelector("#btn");
 
-changeLable.addEventListener("click", switchLable)
+// changeLable.addEventListener("click", switchLable)
 
-function switchLable() {
-    changeLable.button.innerHTML = 'Letters Menu';
-}
-
-
-
+// function switchLable() {
+//     changeLable.button.innerHTML = 'Letters Menu';
+// }
 
 // function click(event)
 // event.preventDefault();
-
-
